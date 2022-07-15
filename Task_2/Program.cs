66377f99f120e4f41652d73f32514545f5ef2f75
Aleksey <busido_na87@mail.ru>
2022-07-15 21:26:47 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Задача№13");
+Console.WriteLine("Введи число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+N = Math.Abs(N);
+if(N < 100)
+{
+Console.WriteLine("Третьей цифры числа N нет");
+}
+else
+{
+    while(N >= 999)
+    {
+        N = N / 10;  
+    }
+Console.WriteLine($"Третья цифра числа N = {N % 10}");  
+}
