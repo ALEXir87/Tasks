e0132363fc66965398e799e5daf19a9ca3b7ca5e
Aleksey <busido_na87@mail.ru>
2022-07-15 17:09:23 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Задача№10");
+Console.WriteLine("Введи трёхзначное число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int digital1 = N / 100;
+int digital2 = N / 10 % 10;
+int digital3 = N % 10;
+Console.WriteLine($"Вторая цифра числа N = {digital2}");
